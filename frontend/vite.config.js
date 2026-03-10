424336226f3c9ad2c985b9081c61db9c916cdd71
shubhranshux <shubhranshu1012@gmail.com>
2026-03-10 22:43:19 +0530

    backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://jobscrapper-i2wz.onrender.com',
         changeOrigin: true
       }
     }
